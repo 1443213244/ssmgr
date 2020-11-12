53cecc5226428a0ba6a7218ddcfaf180562d32e7
1443213244 <1443213244@qq.com>
2020-11-12 17:06:15 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -4,6 +4,7 @@
 echo "编译环境"
  if [[ -f /etc/redhat-release ]]; then
         release="centos"
+	yum –y update
         yum install epel-release -y
 	yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel mbedtls-devel iptables-services -y
         systemPackage="yum"
